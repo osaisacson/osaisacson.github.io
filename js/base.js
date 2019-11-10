cebb07818d35305d3d619228c9e18f43f723fe28
Osa Isacson <asaisacson@gmail.com>
2019-11-10 01:49:50 +0100

    experiment with scrolls

---
+++ js/base.js
@@ -0,0 +1,26 @@
+/*global $, jQuery, alert*/
+$(document).ready(function() {
+  'use strict';
+
+  // ========================================================================= //
+  //  //PARALLAX
+  // ========================================================================= //
+  console.clear();
+
+  const app = document.querySelector('.app');
+  const elNav = document.querySelector('.ui-nav');
+
+  app.addEventListener('scroll', () => {
+    const scroll = (2 * app.scrollTop) / app.clientHeight;
+    app.style.setProperty('--scroll', scroll);
+
+    if (scroll >= 0.01) {
+      if (elNav.className.indexOf('-expanded') < 0) {
+        console.log('adding class');
+        elNav.classList.add('-expanded');
+      }
+    } else {
+      elNav.classList.remove('-expanded');
+    }
+  });
+});
